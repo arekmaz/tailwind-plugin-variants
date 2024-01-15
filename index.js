4161db675ced70db230eecc60f9af280b411f142
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-01-15 16:45:54 +0100

    --wip-- [skip ci]

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/*!
+ * Portions of this file are based on code from headlessui.
+ * MIT Licensed, Copyright (c) 2020 Tailwind Labs.
+ *
+ * Credits to the Tailwind Labs team:
+ * https://github.com/tailwindlabs/headlessui/blob/8e1e19f94c28af68c05becc80bf89575e1fa1d36/packages/@headlessui-tailwindcss/src/index.ts
+ * and Kobalte Team:
+ * https://github.com/kobaltedev/kobalte/blob/05e043ae41c17c2e7735573440d06eb82e9bfff3/packages/tailwindcss/src/index.ts
+ */
+
 const plugin = require("tailwindcss/plugin");
 
 /**
